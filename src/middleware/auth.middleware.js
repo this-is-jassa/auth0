b6bd6271c15f8e70402536e15487cefed8d08a8f
this-is-jassa <jassa.developer@gmail.com>
2020-06-10 01:50:50 -0400

    changes

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,8 +4,8 @@ const userModel = require('../models/user.model');
 const messages = require('../../env/variables');
 const { registerValidation, loginValidation } = require('../helpers/user.validation');
 const config = require('../../env/config');
-const { addSession } = require('../helpers/sessionHandler');
-
+const { addSession, removeSession, removeSessions } = require('../helpers/sessionHandler');
+const jwt = require('jsonwebtoken');
 
 login = async (req, res) => {
     const { error } = loginValidation.validate(req.body);
@@ -34,6 +34,7 @@ login = async (req, res) => {
 }
 
 register = async (req, res) => {
+
     const { error } = registerValidation.validate(req.body);
     if (error) return res.status(400).send({ message: error.details[0].message });
 
@@ -63,6 +64,29 @@ register = async (req, res) => {
 
 }
 
+logout = async (req, res) => {
+
+    const fromEverywhere = true;
+
+    const webToken = req.cookie['auth-token'];
+    if (!webToken) return res.status(400).send({ message: messages.LOGOUT_WITHOUT_TOKEN });
+
+    try {
+        const { _id } = jwt.verify(webToken, config.jwtSecretKey);
+
+        const user = await userModel.findById(_id);
+        user.password = '';
+        const sessions = user.sessions;
+
+        if (fromEverywhere ? await removeSessions(user) : await removeSession(user, sessions, webToken));
+
+        res.clearCookie('auth-token').send({});
+
+    } catch (err) {
+        res.clearCookie('auth-token').status(400).message({ message: messages.INVALID_TOKEN });
+    }
+}
+
 module.exports.login = login;
 module.exports.register = register;
-
+module.exports.logout = logout;
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,9 @@
-const {login, register} = require('../middleware/auth.middleware');
+const {login, register, logout} = require('../middleware/auth.middleware');
 const Router = require('express').Router()
 
 
 Router.post('/login', login);
-Router.post('/register', register)
+Router.post('/register', register);
+Router.post('/logout', logout);
 
 module.exports = Router;
